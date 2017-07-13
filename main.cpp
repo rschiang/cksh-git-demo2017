7e9541a3e77be0d42c03db47d62df92c893c4a22
Poren Chiang <ren.chiang@gmail.com>
2017-07-13 15:27:56 +0800

    Initial commit on my first project

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  for (int i = 0; i < 10; i++) {
+    for (int j = i; j < 10; j++)
+      cout << "*";
+    cout << endl;
+  }
+  return 0;
+}
