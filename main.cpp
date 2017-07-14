108de66a4be8fa80b4da523104dd6afeb742bf45
segnoda <segno.dada@gmail.com>
2017-07-14 14:12:45 +0800

    fixed double quotes

--- main.cpp
+++ main.cpp
@@ -4,10 +4,10 @@ using namespace std;
 int main() {
   for (int i = 0; i < 20; i++) {
     for (int j = i; j < 20; j++)
-      cout << “O”;
+      cout << "O";
     cout << endl;
   }
-  cout << “學號：09930334” << endl;
+  cout << "學號：09930334" << endl;
   cout << "您蒸蚌！(・ω・)" << endl;
   return 0;
 }
